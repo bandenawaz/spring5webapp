25cac700b45fd9644df28fa31e11bb34d845a364
Bandenawaz Bagwan <bandenawaz.bagwan.com>
2023-08-11 21:24:19 +0530

    Added MVC Controller

--- src/main/java/com/illuminateskiils/spring5webapp/controller/BookController.java
+++ src/main/java/com/illuminateskiils/spring5webapp/controller/BookController.java
@@ -3,7 +3,9 @@ package com.illuminateskiils.spring5webapp.controller;
 import com.illuminateskiils.spring5webapp.repositories.BookRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class BookController {
@@ -14,10 +16,10 @@ public class BookController {
         this.bookRepository = bookRepository;
     }
 
-    @RequestMapping("/books")
+    @RequestMapping(value = "/books")
     public String getBooks(Model model){
 
         model.addAttribute("books", bookRepository.findAll()) ;
-        return "book";
+        return "list";
     }
 }
